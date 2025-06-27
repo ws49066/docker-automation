d498e39295700cb829824570466abb7a92fe640f
Danilo de Souza Miguel <68231382+Danilo-Miguel@users.noreply.github.com>
2025-06-27 16:04:32 -0300

    feat: initialize Flask authentication service

---
+++ burguer-app/auth-service/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, session
+from controllers.auth_controller import auth_bp
+from dotenv import load_dotenv
+import os
+
+# Carrega as variáveis de ambiente do arquivo .env
+load_dotenv()
+
+# Cria a instância da aplicação Flask
+app = Flask(__name__)
+
+secret_key = os.getenv("SECRET_KEY")
+
+print(f"SECRET_KEY carregada: {secret_key}")  # DEBUG: deve imprimir o valor correto
+
+
+
+#Define a chave secreta para a sessão
+app.secret_key = os.getenv("SECRET_KEY")
+
+
+# Registra o blueprint de autenticação
+app.register_blueprint(auth_bp, url_prefix='/auth')
+
+# Redireciona a rota raiz para a página de login
+@app.route('/')
+def index():
+    return '<a href="/auth/login">Ir para o login</a>'
+
+if __name__ == '__main__':
+    # Executa a aplicação Flask
+    app.run(port=5000, debug=True)
+    # O debug=True permite recarregar automaticamente a aplicação ao fazer alterações no código

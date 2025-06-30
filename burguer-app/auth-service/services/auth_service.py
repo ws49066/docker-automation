be7087fd25b941f8a139671838e297c6c8bfcaa3
Danilo de Souza Miguel <68231382+Danilo-Miguel@users.noreply.github.com>
2025-06-30 16:00:53 -0300

    refactor(microservices): implement single responsibility in auth service

--- burguer-app/auth-service/services/auth_service.py
+++ burguer-app/auth-service/services/auth_service.py
@@ -1,32 +1,18 @@
-from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import check_password_hash
 from config.database import get_db
+from utils.jwt_handler import generate_token
 
-# Obtém a referênia da coleção de usuários
 db = get_db()
-user_collection = db["users"]
+users_col = db["users"]
 
-" Realiza o cadastro de um usuário no banco de dados"
-
-def create_user(email, password, role):
-    # Verifica se o usuário já existe
-    if user_collection.find_one({"email": email}):
-        return {"error": "User already exists"}, 400
-    
-    " Cria o usuário com a senha criptografada"
-    hashed_password = generate_password_hash(password)
-    user = {
-        "email": email,
-        "password": hashed_password,
-        "role": role
-    }
-
-    # Insere o usuário no banco de dados
-    user_collection.insert_one(user)
-    return {"message": "Usuário criado com sucesso"}, 201
-
-# Realiza o login do usuário verificando as credenciais
 def login_user(email, password):
-    user = user_collection.find_one({"email": email})
+    user = users_col.find_one({"email": email})
     if not user or not check_password_hash(user["password"], password):
-        return {"error": "Invalid credentials"}, 401
-    return user
+        return None
+    token = generate_token(user["email"], user["role"])
+    # Retorna token e os dados do usuário para a sessão
+    return {
+        "email": user["email"],
+        "role": user.get("role", "cliente"),
+        "token": token
+    }
